cd9de159484a28b787775eec00a931b9a1eae664
caleonardo <65500442+caleonardo@users.noreply.github.com>
2026-08-01 22:44:19 +0100

    Update and rename setup.sh to setup_bash_manually__non_nixos.sh

--- setup_bash_manually__non_nixos.sh
+++ setup_bash_manually__non_nixos.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
+echo "Configures Bash Colors, commands lookup and eternal history."
+
 set -euo pipefail
 
 # Base URL to your raw GitHub repository files
 REPO_RAW_URL="https://raw.githubusercontent.com/caleonardo/bash_settings/refs/heads/main"
 
 
-# Create a temporary directory for downloaded modules
+echo "Creating a temporary directory for downloaded modules"
 WORKDIR=$(mktemp -d)
 trap 'rm -rf "$WORKDIR"' EXIT
 
-# 1. Download helper scripts and configs
+echo "1. Download helper scripts and configs"
 curl -fsSL "${REPO_RAW_URL}/_1_terminal_colors.sh" -o "${WORKDIR}/_1_terminal_colors.sh"
 curl -fsSL "${REPO_RAW_URL}/_3_eternal_history.sh" -o "${WORKDIR}/_3_eternal_history.sh"
 curl -fsSL "${REPO_RAW_URL}/.inputrc" -o "${WORKDIR}/.inputrc"
 
-# 2. Create Local dir and copy files
+echo "2. Create Local dir and copy files in ~/.bash_profile__custom_config_files"
 mkdir ~/.bash_profile__custom_config_files
 cp "${WORKDIR}/_1_terminal_colors.sh" ~/.bash_profile__custom_config_files/_1_terminal_colors.sh
 cp "${WORKDIR}/_3_eternal_history.sh" ~/.bash_profile__custom_config_files/_3_eternal_history.sh
-cat "${WORKDIR}/.inputrc" >> ~/.inputrc
 
-# 3. Updating user ~/.profile
+echo "3. Updating user ~/.profile"
 echo "# " >> ~/.profile
 echo "# Configure Terminal colors" >> ~/.profile
 echo "source ~/.bash_profile__custom_config_files/_1_terminal_colors.sh" >> ~/.profile
@@ -30,8 +31,8 @@ echo "source ~/.bash_profile__custom_config_files/_3_eternal_history.sh" >> ~/.p
 # shellcheck disable=SC1090
 source ~/.profile
 
-# 4. Updating user ~/.inputrc
-bind -d ~/.inputrc
+echo "4. Updating user ~/.inputrc"
+cat "${WORKDIR}/.inputrc" >> ~/.inputrc
+bind -f ~/.inputrc
 
-
-echo "Main setup completed successfully."
+echo "Setup completed successfully."
