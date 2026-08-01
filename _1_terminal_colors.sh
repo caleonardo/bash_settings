0085af45cc6398f27a66b64bfdd84f26fda3381b
caleonardo <65500442+caleonardo@users.noreply.github.com>
2026-08-01 22:24:52 +0100

    Update _1_terminal_colors.sh

--- _1_terminal_colors.sh
+++ _1_terminal_colors.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+# shellcheck disable=SC2148
 # Make sure variables are declared before use
 set -u
 
@@ -37,19 +36,3 @@ _CURRENT_DIR_FULL_PATH="\n${PS1_BOLD_BLUE}\w${PS1_COLOR_RESET}\n"
 _CURRENT_DIR_NAME="${PS1_BOLD_BLUE}\W${PS1_COLOR_RESET}"
 
 export PS1="${_CURRENT_DIR_FULL_PATH}${PS1_BOLD_YELLOW}${PS1_DATE}_${PS1_TIME} ${PS1_BOLD_GREEN}${PS1_USERNAME}@${PS1_HOSTNAME}:${_CURRENT_DIR_NAME}:\$ "
-# ============================= ETERNAL BASH HISTORY =====================================
-# ---------------------
-# Undocumented feature which sets the size to "unlimited".
-# http://stackoverflow.com/questions/9457233/unlimited-bash-history
-export HISTFILESIZE=-1
-export HISTSIZE=-1
-export HISTTIMEFORMAT="[%F %T] "
-# Change the file location because certain bash sessions truncate .bash_history file upon close.
-# http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
-# export HISTFILE=/data/tech_labs/settings/linux/nixos/settings_nixos/users/_common/dotfiles/.bash_eternal_history
-# Force prompt to write history after every command.
-# Documented in https://unix.stackexchange.com/questions/767621/i-cant-get-bash-history-to-update-instantly-in-all-terminals
-# Ignore unset variables
-set +u
-original_prompt_command=${original_prompt_command-$PROMPT_COMMAND}
-PROMPT_COMMAND="history -a; history -n; $original_prompt_command"
