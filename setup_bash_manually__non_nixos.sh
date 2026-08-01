3c8fee43d15875579443f7438a49b3902733d3c2
caleonardo <65500442+caleonardo@users.noreply.github.com>
2026-08-01 22:51:26 +0100

    Update setup_bash_manually__non_nixos.sh

--- setup_bash_manually__non_nixos.sh
+++ setup_bash_manually__non_nixos.sh
@@ -7,7 +7,6 @@ set -euo pipefail
 # Base URL to your raw GitHub repository files
 REPO_RAW_URL="https://raw.githubusercontent.com/caleonardo/bash_settings/refs/heads/main"
 
-
 echo "Creating a temporary directory for downloaded modules"
 WORKDIR=$(mktemp -d)
 trap 'rm -rf "$WORKDIR"' EXIT
@@ -18,21 +17,22 @@ curl -fsSL "${REPO_RAW_URL}/_3_eternal_history.sh" -o "${WORKDIR}/_3_eternal_his
 curl -fsSL "${REPO_RAW_URL}/.inputrc" -o "${WORKDIR}/.inputrc"
 
 echo "2. Create Local dir and copy files in ~/.bash_profile__custom_config_files"
-mkdir ~/.bash_profile__custom_config_files
+mkdir -p ~/.bash_profile__custom_config_files
 cp "${WORKDIR}/_1_terminal_colors.sh" ~/.bash_profile__custom_config_files/_1_terminal_colors.sh
 cp "${WORKDIR}/_3_eternal_history.sh" ~/.bash_profile__custom_config_files/_3_eternal_history.sh
 
 echo "3. Updating user ~/.profile"
-echo "# " >> ~/.profile
-echo "# Configure Terminal colors" >> ~/.profile
-echo "source ~/.bash_profile__custom_config_files/_1_terminal_colors.sh" >> ~/.profile
-echo "source ~/.bash_profile__custom_config_files/_3_eternal_history.sh" >> ~/.profile
-
-# shellcheck disable=SC1090
-source ~/.profile
+{
+    echo ""
+    echo "# Configure Terminal colors & eternal history"
+    echo "source ~/.bash_profile__custom_config_files/_1_terminal_colors.sh"
+    echo "source ~/.bash_profile__custom_config_files/_3_eternal_history.sh"
+} >> ~/.profile
 
 echo "4. Updating user ~/.inputrc"
 cat "${WORKDIR}/.inputrc" >> ~/.inputrc
-bind -f ~/.inputrc
 
+echo ""
 echo "Setup completed successfully."
+echo "Run 'source ~/.profile' or restart your terminal session to apply changes."
+echo "Run 'bind -f ~/.inputrc' or restart your terminal session to apply changes."
